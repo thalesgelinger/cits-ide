3235ef40dc6f65cfb4758ed272ed82ad12f9a62c
Thales Gelinger <thalesgelinger@gmail.com>
2023-07-11 17:06:53 -0300

    chore

--- pages/ide/test.tsx
+++ pages/ide/test.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function test() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Text style={styles.text}>Welcome to Expo - Next.js 👋</Text>
     </View>
   );
 }
